3bd3bcf2473e6f94bbeadfb603e5a280257215a8
El Chema <106692495+GyutaroNc@users.noreply.github.com>
2022-06-12 02:05:38 -0600

    𝑺𝑯𝑨𝑹𝑲 𝑩𝑶𝑻ㅤ🦈

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix }) => {
-let pp = 'https://i.imgur.com/WHjtUae.jpg'
+let pp = 'https://i.ibb.co/M7LxSfh/Black-Shark.jpg'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 try {
 pp = await conn.getProfilePicture(who)
