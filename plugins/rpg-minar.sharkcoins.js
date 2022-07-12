e4188bee44351add2b0b197c0fed0fe682c20ed3
El Chema <106692495+GyutaroNc@users.noreply.github.com>
2022-07-12 11:15:45 -0600

    𝑺𝑯𝑨𝑹𝑲 𝑩𝑶𝑻ㅤ🦈

--- plugins/rpg-minar.sharkcoins.js
+++ plugins/rpg-minar.sharkcoins.js
@@ -32,9 +32,9 @@ seconds = Math.floor((duration / 1000) % 60),
 minutes = Math.floor((duration / (1000 * 60)) % 60),
 hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 
-hours = (hours < 10) ? "0" + hours : hours
-minutes = (minutes < 10) ? "0" + minutes : minutes
-seconds = (seconds < 10) ? "0" + seconds : seconds
+hours = (hours < 0) ? "0" + hours : hours
+minutes = (minutes < 0) ? "0" + minutes : minutes
+seconds = (seconds < 0) ? "0" + seconds : seconds
 
 return minutes + " m y " + seconds + " s " 
 }  
